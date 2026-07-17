9ae2eef0e1e291598a8a07e3d0cdf73105a8fa46
Jacky Lee <JackyLeeHongJian@Gmail.com>
2026-07-17 11:36:37 +0800

    Update the css selector for directors and stars

--- download.js
+++ download.js
@@ -2,8 +2,8 @@
 import fs from 'fs';
 import { Readable } from 'stream';
 import puppeteer from 'puppeteer-core';
-const date = '2026-06-07';
-const tt = 'tt30144839';
+const date = '2026-07-17';
+const tt = 'tt14905854';
 const directory = `assets/${date} ${tt}`;
 await fs.promises.mkdir(directory, { recursive: true });
 const browser = await puppeteer.launch({
@@ -14,6 +14,7 @@ const page = await browser.newPage();
 await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64; rv:133.0) Gecko/20100101 Firefox/133.0');
 await page.setExtraHTTPHeaders({'accept-language': 'en,en-US;q=0.9,zh-CN;q=0.8,zh-TW;q=0.7,zh;q=0.6'}); // This language header will affect the returned value of title and the returned url of poster image.
 await page.goto(`https://www.imdb.com/title/${tt}/`, { waitUntil: 'networkidle0' });
+await new Promise(resolve => setTimeout(resolve, 1400));
 const cast = await page.$('div.title-cast__grid > div.ipc-shoveler__grid');
 await cast.evaluate(div => {
 	const elements = div.querySelectorAll('div.sc-10bde568-5');
@@ -25,8 +26,8 @@ const title = await page.$eval('span.hero__primary-text', el => el.innerText);
 let year = await page.$eval('ul.sc-b41e510f-3 > li', el => el.innerText); // Sometimes 'TV Movie' is returned. In this case, get the second <li>.
 if (year.length !== 4) year = await page.$eval('ul.sc-b41e510f-3 > li:nth-child(2)', el => el.innerText);
 const plot = await page.$eval('span.sc-9a16f31-2', el => el.innerText);
-const directors = await page.$$eval('div.sc-af040695-3 > ul > li:nth-child(1) > div > ul > li', elements => elements.map(el => el.innerText));
-const stars = await page.$$eval('div.sc-af040695-3 > ul > li:nth-child(3) > div > ul > li', elements => elements.map(el => el.innerText));
+const directors = await page.$$eval('div.sc-dcbc0103-3 > ul > li:nth-child(1) > div > ul > li', elements => elements.map(el => el.innerText));
+const stars = await page.$$eval('div.sc-dcbc0103-3 > ul > li:nth-child(3) > div > ul > li', elements => elements.map(el => el.innerText));
 const posterUrl = await page.$eval('img.ipc-image', el => el.src).then(url => `${url.split('_')[0]}jpg`);
 const previewUrl = await page.$eval('div.jw-preview', el => el.style.cssText.split('"')[1]).then(url => `${url.split('_')[0]}jpg`).catch(e => undefined);
 let trailerUrl;
