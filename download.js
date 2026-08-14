057f512cec4903c4194eae1378f1689376d22fb6
Jacky Lee <JackyLeeHongJian@Gmail.com>
2026-08-14 14:18:27 +0800

    Add M3GAN 2.0

--- download.js
+++ download.js
@@ -2,8 +2,8 @@
 import fs from 'fs';
 import { Readable } from 'stream';
 import puppeteer from 'puppeteer-core';
-const date = '2026-07-28';
-const tt = 'tt11655566';
+const date = '2026-08-14';
+const tt = 'tt26342662';
 const directory = `assets/${date} ${tt}`;
 await fs.promises.mkdir(directory, { recursive: true });
 const browser = await puppeteer.launch({
